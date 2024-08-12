cbc423964d3b16b54a48effedf4b3c350fc793c1
Naysah Sheikh <135449013+the-nayshe-5@users.noreply.github.com>
2024-08-13 00:57:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,7 +13,8 @@ def get_data(ticker, start_date, end_date, period):
     data = yf.download(ticker, start = start_date, end = end_date, interval = period)
     stocks = yf.Ticker(ticker)
     # st.write(data)
-    col = st.selectbox('Select Option:', ["Open", "Close", "Adj Close"])
+    # col = st.selectbox('Select Option:', ["Open", "Close", "Adj Close"])
+    col = "Open"
     fig1 = px.line(data, x = data.index, y = data[col], title = ticker + " Stocks")
     st.plotly_chart(fig1)
     return [data, stocks]
